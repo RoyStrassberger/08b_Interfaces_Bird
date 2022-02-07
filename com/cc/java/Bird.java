664a3a7d6252590d3614583ebf2fc8ebc028e91b
RoyStrassberger <rdf3001@googlemail.com>
2022-02-07 09:07:53 +0100

    update

--- com/cc/java/Bird.java
+++ com/cc/java/Bird.java
@@ -1,6 +1,19 @@
 package com.cc.java;
 
-public class Bird{
+public class Bird implements Flyable, IFeathers
+{
+
+    @Override
+    public String fly() {
+        // TODO Auto-generated method stub
+        return "I'm a Bird, I can fly.";
+    }
+
+    @Override
+    public String hasFeathers() {
+        // TODO Auto-generated method stub
+        return "I'm a bird, I have feathers.";
+    }
 
     
 }
